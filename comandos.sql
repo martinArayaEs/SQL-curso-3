f0cb04e01a2a53f66e1c087ee2246a075f59a8c9
Martin Araya <95448275+martinArayaEs@users.noreply.github.com>
2022-10-26 13:53:55 -0300

    Update comandos.sql
    Arreglo codigo introduccion

--- comandos.sql
+++ comandos.sql
@@ -1,16 +1,3 @@
-/*En este archivo .sql encontrarás todos los comandos que ejecutaremos durante el desarrollo de nuestro entrenamiento. 
-
-Por el momento, te invitamos a preparar el ambiente para que puedas desarrollar todos los ejercicios. 
-
-¡Te deseo muchos éxitos en tus estudios!*/
-
-/*COMANDOS AULA 1
-
-A partir de este punto, estás listo para trabajar directamente en Workbench.
-
-Debes ver todos los videos referentes al Aula 2 y colocar en práctica lo que estarás aprendiendo.
-
-¡Mucho ánimo! Nuestro curso ha comenzado.*/
 
 /*COMANDOS AULA 2*/
 
